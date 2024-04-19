5eae9337e7b3e0c84ed9f37bda6e21a957522177
Anil Kumar <113283470+Jerry20025@users.noreply.github.com>
2024-04-19 12:36:02 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://ak2498315:omlVj4L5V9W7H5ny@cluster0.qeunvkq.mongodb.net/email")
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        trim: true
+    },
+    location: String,
+    age: String,
+    workDetails: String,
+    otp:String
+});
+
+
+const User= mongoose.model('User', userSchema);
+module.exports ={
+    User,
+}
